d6036e68902eae9569c4d0423c9980e952a05418
Twopidee <42394451+Twopidee@users.noreply.github.com>
2019-01-22 17:01:42 -0600

    Update keymap.c
    Added Layer 2 for Macros

--- pickles/ebdz60/keymap.c
+++ pickles/ebdz60/keymap.c
@@ -6,6 +6,12 @@
 #define _L1 1
 #define _L2 2
 
+//Macros
+enum custom_keycodes {
+  M_CUNT = SAFE_RANGE,
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // 0: Base Layer
@@ -19,15 +25,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // 1: Function Layer
 		[_L1] = LAYOUT_60_ansi(
       KC_GRV, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, LCA_T(KC_DEL),
-      KC_NO,  KC_NO,   KC_UP,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
+      KC_NO,  KC_NO,   KC_UP,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, TO(2),
       KC_NO,  KC_LEFT, KC_DOWN, KC_RGHT,  BL_TOGG, BL_INC,  BL_DEC,  BL_STEP, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
       KC_NO,  RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD,
       KC_NO,  KC_NO,   KC_NO,                      KC_NO,                              KC_NO,   KC_TRNS, KC_NO, KC_NO)
 
 	 [_L2] = LAYOUT_60_ansi(
-					TO(0),  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+					TO(0),  M_CUNT, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 					KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 					KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 					KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 					KC_NO,  KC_NO, KC_NO,                      KC_NO,                              KC_NO, KC_NO, KC_NO, KC_NO)
+};
+
+// Custom Actions
+
+
+ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case M_CUNT:
+      if (record->event.pressed) {
+        // when keycode M_CUNT is pressed
+        SEND_STRING(SS_TAP(X_T))"What a cunt!"(SS_TAP(X_ENT)));
+      } else {
+        // when keycode M_CUNT is released
+      }
+      break;
+
+  }
+  return true;
 };
