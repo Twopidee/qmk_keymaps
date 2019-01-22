953409846e8b854d3ee3463b94a6c76dc5fcb5d7
Twopidee <42394451+Twopidee@users.noreply.github.com>
2019-01-21 23:35:29 -0600

    Updated keymap, added readme

--- pickles/ebdz60/keymap.c
+++ pickles/ebdz60/keymap.c
@@ -4,6 +4,7 @@
 
 #define _L0 0
 #define _L1 1
+#define _L2 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -16,10 +17,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL,  KC_LGUI, KC_LALT,                KC_SPC,                           KC_LALT, MO(1),   KC_LGUI, KC_LCTL),
 
 // 1: Function Layer
-  [_L1] = LAYOUT_60_ansi(
+		[_L1] = LAYOUT_60_ansi(
       KC_GRV, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, LCA_T(KC_DEL),
       KC_NO,  KC_NO,   KC_UP,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
       KC_NO,  KC_LEFT, KC_DOWN, KC_RGHT,  BL_TOGG, BL_INC,  BL_DEC,  BL_STEP, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
       KC_NO,  RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD,
       KC_NO,  KC_NO,   KC_NO,                      KC_NO,                              KC_NO,   KC_TRNS, KC_NO, KC_NO)
+
+	 [_L2] = LAYOUT_60_ansi(
+					TO(0),  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+					KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+					KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+					KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+					KC_NO,  KC_NO, KC_NO,                      KC_NO,                              KC_NO, KC_NO, KC_NO, KC_NO)
 };
---
+++ pickles/ebdz60/readme.md
@@ -0,0 +1,11 @@
+# **PICKLES "EBDZ60" LAYOUT**
+![Imgur](https://i.imgur.com/QT4eLAp.jpg)
+
+
+Layer 0 is default configuration, while layer 1 is function/lighting
+
+Make example for this keyboard (after setting up your build environment):
+
+    make dz60:ebdz60
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
