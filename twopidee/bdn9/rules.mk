53b3df4c9f9b69eeaaa52fe90f917c7afa666181
Twopidee <42394451+Twopidee@users.noreply.github.com>
2019-02-19 02:22:10 -0600

    added twopidee bdn9 keymap

---
+++ twopidee/bdn9/rules.mk
@@ -0,0 +1,4 @@
+CTPC = yes
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
